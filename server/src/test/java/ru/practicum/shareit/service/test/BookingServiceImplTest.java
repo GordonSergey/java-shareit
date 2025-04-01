0aa372f8c1ad201199e95827a221703b744dde1a
gordo <mrgordon1111@gmail.com>
2025-04-01 03:04:28 +0200

    add-item-requests-and-gateway

--- server/src/test/java/ru/practicum/shareit/service/test/BookingServiceImplTest.java
+++ server/src/test/java/ru/practicum/shareit/service/test/BookingServiceImplTest.java
@@ -25,7 +25,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
-public class BookingServiceTest {
+public class BookingServiceImplTest {
 
     @Mock
     private BookingRepository bookingRepository;
